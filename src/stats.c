bcb9137681fa1b80bfd83dd4ecc021304bef6e22
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-03-21 11:21:36 +0100

    Corrections to compile with pedantic option

--- src/stats.c
+++ src/stats.c
@@ -3,11 +3,9 @@
 
 double roofline_output_sd(struct roofline_sample_out * out, unsigned n){
     unsigned i;
-    double mean = 0;
-    double M2 = 0;
-    double delta = 0;
+    double mean = 0, M2 = 0, delta = 0, throughput;
     for(i=0;i<n;i++){
-	double throughput = (double)(out[i].instructions) / (double)(out[i].ts_end - out[i].ts_start);
+	throughput = (double)(out[i].instructions) / (double)(out[i].ts_end - out[i].ts_start);
 	delta = throughput-mean;
         mean += delta/(i+1);
 	M2 += delta*(throughput - mean);
@@ -19,10 +17,12 @@ double roofline_output_sd(struct roofline_sample_out * out, unsigned n){
 }
 
 static int comp_roofline_throughput(void * a, void * b){
-    struct roofline_sample_out a_out = (*(struct roofline_sample_out *)a);
-    struct roofline_sample_out b_out = (*(struct roofline_sample_out *)b);
-    float val_a = a_out.instructions / (float)(a_out.ts_end - a_out .ts_start);
-    float val_b = b_out.instructions / (float)(b_out.ts_end - b_out .ts_start);
+    struct roofline_sample_out a_out, b_out;
+    float val_a, val_b;
+    a_out = (*(struct roofline_sample_out *)a);
+    b_out = (*(struct roofline_sample_out *)b);
+    val_a = a_out.instructions / (float)(a_out.ts_end - a_out .ts_start);
+    val_b = b_out.instructions / (float)(b_out.ts_end - b_out .ts_start);
     if( val_a < val_b )
 	return -1;
     else if(val_a > val_b)
@@ -37,8 +37,12 @@ int roofline_output_median(struct roofline_sample_out * samples, size_t n){
 }
 
 int roofline_output_max(struct roofline_sample_out * samples, size_t n){
-    int ret = 0;
-    struct roofline_sample_out max = samples[--n];
+    int ret;
+    struct roofline_sample_out max;
+
+    ret = 0;
+    max = samples[--n];
+
     while(n--){
 	if(comp_roofline_throughput(&max,&samples[n]) == -1){ 
 	    max = samples[n];
@@ -49,8 +53,11 @@ int roofline_output_max(struct roofline_sample_out * samples, size_t n){
 }
 
 int roofline_output_min(struct roofline_sample_out * samples, size_t n){
-    int ret = 0;
-    struct roofline_sample_out min = samples[--n];
+    int ret;
+    struct roofline_sample_out min;
+
+    ret = 0;
+    min = samples[--n];
     while(n--){
 	if(comp_roofline_throughput(&min,&samples[n]) == 1){ 
 	    min = samples[n];
@@ -60,11 +67,14 @@ int roofline_output_min(struct roofline_sample_out * samples, size_t n){
     return ret;
 }
 
-long long roofline_autoset_loop_repeat(void (* bench_fun)(struct roofline_sample_in *, struct roofline_sample_out *), struct roofline_sample_in * in, long long ms_dur){
-    in->loop_repeat = 1; 
+long roofline_autoset_loop_repeat(void (* bench_fun)(struct roofline_sample_in *, struct roofline_sample_out *), struct roofline_sample_in * in, long ms_dur){
+    float mul;
+    long tv_ms;
     struct roofline_sample_out out;
-    float mul = 0;
-    long long tv_ms = 0;
+
+    in->loop_repeat = 1; 
+    mul = 0;
+    tv_ms = 0;
     while(tv_ms < ms_dur){
 	bench_fun(in,&out);
 	tv_ms = (out.ts_end-out.ts_start)*1000/cpu_freq;
@@ -74,7 +84,7 @@ long long roofline_autoset_loop_repeat(void (* bench_fun)(struct roofline_sample
 	}
 
 	else if( tv_ms < ms_dur ){
-	    if(in->loop_repeat == (long long)(in->loop_repeat * mul))
+	    if(in->loop_repeat == (long)(in->loop_repeat * mul))
 		in->loop_repeat += 1;
 	    else
 		in->loop_repeat *= mul;
@@ -91,7 +101,10 @@ roofline_repeat_bench(void (* bench_fun)(struct roofline_sample_in *, struct roo
 		      int (* bench_stat)(struct roofline_sample_out * , size_t))
 {
     unsigned i;
-    struct roofline_sample_out * samples; roofline_alloc(samples,sizeof(*samples)*BENCHMARK_REPEAT);
+    struct roofline_sample_out * samples; 
+    double sd; 
+
+    roofline_alloc(samples,sizeof(*samples)*BENCHMARK_REPEAT);
     for(i=0;i<BENCHMARK_REPEAT;i++){
 	samples[i].ts_start = 0;
 	samples[i].ts_end = 0;
@@ -102,7 +115,7 @@ roofline_repeat_bench(void (* bench_fun)(struct roofline_sample_in *, struct roo
     }    
     /* sort results */
     *out = samples[bench_stat(samples, i)];
-    double sd = roofline_output_sd(samples,i);
+    sd = roofline_output_sd(samples,i);
     free(samples);
     return sd;
 }
