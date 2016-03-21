2cc8abab76930c19544a675cbaa530242c87859c
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-03-21 11:21:30 +0100

    Corrections to compile with pedantic option

--- src/main.c
+++ src/main.c
@@ -14,7 +14,9 @@ void usage(char * argv0){
 }
 
 void parse_args(int argc, char ** argv){
-    int i = 0;
+    int i;
+
+    i= 0;
     while(++i < argc){
 	if(!strcmp(argv[i],"--help") || !strcmp(argv[i],"-h"))
 	    usage(argv[0]);
@@ -37,15 +39,19 @@ void parse_args(int argc, char ** argv){
 }
 
 FILE * open_output(char * out){
+    FILE * fout;
+
     if(out == NULL)
 	return NULL;
-    FILE * fout = fopen(out,"w");
+    fout = fopen(out,"w");
     if(fout == NULL) perrEXIT("fopen");
     return fout;
 }
 
 void roofline_mem_bench(FILE * out, hwloc_obj_t memory){
-    double oi = 0;
+    double oi;
+
+    oi = 0;
     /* Benchmark load */
     if(load){
 	roofline_bandwidth(out, memory, ROOFLINE_LOAD);
@@ -65,16 +71,20 @@ void roofline_mem_bench(FILE * out, hwloc_obj_t memory){
 }
 
 int main(int argc, char * argv[]){
+    FILE * out;
+    char info[128];
+
+    memset(info,0,sizeof(info));
+
     if(roofline_lib_init()==-1)
 	errEXIT("roofline library init failure");
 
     parse_args(argc,argv);
     
-    FILE * out = open_output(output);
+    out = open_output(output);
     if(out==NULL) out = stdout;
 
     /* print header */
-    roofline_mkstr_stack(info,128);
 #ifdef USE_OMP
     snprintf(info,sizeof(info), "%5s %5s", "info", "scale");
     roofline_print_header(out, info);
