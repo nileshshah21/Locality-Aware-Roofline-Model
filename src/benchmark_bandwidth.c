1ee3198a6f3b50fd762b8317b0982980f484d2d5
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-03-22 09:42:42 +0100

    test every number of threads

--- src/benchmark_bandwidth.c
+++ src/benchmark_bandwidth.c
@@ -7,44 +7,58 @@
 #define BENCHMARK_N_SAMPLE 32
 #define OUTPUT stdout
 
+char info[128];
 void (* bandwidth_bench) (struct roofline_sample_in * in, struct roofline_sample_out * out);    
 
-struct roofline_sample_out empty_out(){
-    struct roofline_sample_out output = {0,0,0,0,0};
-    return output;
-}
-
 static void run_bench(hwloc_obj_t memory){
-    roofline_hwloc_membind(memory);
-#ifdef USE_OMP
-    omp_set_num_threads(hwloc_get_nbobjs_inside_cpuset_by_type(topology,memory->cpuset, HWLOC_OBJ_CORE));
-#endif
-    /* Set the bounds size */
-    hwloc_obj_t prev = roofline_hwloc_get_previous_memory(memory);
-    while(roofline_hwloc_get_memory_size(prev) >= roofline_hwloc_get_memory_size(memory))
-	prev = roofline_hwloc_get_previous_memory(memory);
-    size_t min = roofline_MAX(1024,4*roofline_hwloc_get_memory_size(prev));
-    if(memory->type == HWLOC_OBJ_NODE)
-	min *= 4;
-    size_t max = roofline_MIN(LLC_size*64, roofline_hwloc_get_memory_size(memory));
-    struct roofline_sample_in  input = {1,NULL, 0};
+    unsigned n_child;
+    size_t min, max;
+    struct roofline_sample_in  input;
     struct roofline_sample_out output;
-    alloc_chunk_aligned(&input.stream, max);
-
-    int i, n_sizes = BENCHMARK_N_SAMPLE;
-    size_t * sizes = roofline_log_array(min, max, &n_sizes);
+    int i, n_sizes;
+    size_t * sizes;
     double sd;
+    unsigned n_threads, tid;
+    hwloc_obj_t child;
+    
+    roofline_hwloc_membind(memory);
 
-    for(i=0;i<n_sizes;i++){ 
-	output = empty_out();
-	input.stream_size = alloc_chunk_aligned(NULL,sizes[i]);
-	roofline_autoset_loop_repeat(bandwidth_bench, &input, BENCHMARK_MIN_DUR);
-	sd = roofline_repeat_bench(bandwidth_bench, &input, &output, roofline_output_median);
-	roofline_mkstr_stack(info,128); 
-	snprintf(info,sizeof(info),"%10zu", sizes[i]);
-	roofline_print_sample(OUTPUT, memory, &output, sd, info);
+#ifdef USE_OMP
+    n_threads = hwloc_get_nbobjs_inside_cpuset_by_type(topology,memory->cpuset, HWLOC_OBJ_CORE);
+#else
+    tid = 1; n_threads = 1;
+#endif
+
+    /* Set lower bound size as 4 times the sum of under memories size */
+    child  = roofline_hwloc_get_previous_memory(memory);
+    if(child != NULL){
+	n_child = hwloc_get_nbobjs_inside_cpuset_by_depth(topology, memory->cpuset, child->depth);
+	min = 4*n_child*roofline_hwloc_get_memory_size(child);
     }
+    else
+	min = 1024;
+    max = roofline_MIN(LLC_size*16, roofline_hwloc_get_memory_size(memory));
 
+
+    alloc_chunk_aligned(&input.stream, max);
+    n_sizes = BENCHMARK_N_SAMPLE;
+    sizes = roofline_log_array(min, max, &n_sizes);
+#ifdef USE_OMP
+    for(tid=1; tid<=n_threads; tid++){ 
+	omp_set_num_threads(tid);
+#endif
+	for(i=0;i<n_sizes;i++){ 
+	    roofline_output_clear(&output);
+	    input.stream_size = alloc_chunk_aligned(NULL,sizes[i]);
+	    roofline_autoset_loop_repeat(bandwidth_bench, &input, BENCHMARK_MIN_DUR);
+	    sd = roofline_repeat_bench(bandwidth_bench, &input, &output, roofline_output_median);
+	    memset(info, 0, sizeof(info));
+	    snprintf(info,sizeof(info),"%10zu", sizes[i]);
+	    roofline_print_sample(OUTPUT, memory, &output, sd, tid, info);
+	}
+#ifdef USE_OMP
+    }
+#endif
     free(input.stream);
     free(sizes);
 }
@@ -70,7 +84,7 @@ int main(int argc, char ** argv){
 	    usage(argv[0]);
     }
 
-    roofline_mkstr_stack(info,128); sprintf(info, "%10s", "size");
+    memset(info, 0, sizeof(info));
     roofline_print_header(OUTPUT, info);
     
     if(mem == NULL){
