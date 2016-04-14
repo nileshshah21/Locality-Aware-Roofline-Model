013c1c266a491c5d6ddead849e17b9e7dbf9e14b
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-04-14 11:34:17 +0200

    enable benchmark of a single operational intensity for debug prupose

--- src/main.c
+++ src/main.c
@@ -8,10 +8,11 @@ char * mem_str = NULL;
 hwloc_obj_t mem = NULL;
 int load = 0, store = 0;
 int hyperthreading = 0;
+float oi = -1;
 
 void usage(char * argv0){
-    printf("%s -h  -v -l -s -m <hwloc_ibj:idx> -o <output> -ht\n", argv0);
-    printf("%s --help --validate --load --store --memory <hwloc_obj:idx> --output <output> --with-hyperthreading\n", argv0);
+    printf("%s -h  -v -l -s -m <hwloc_ibj:idx> -o <output> -ht --oi <operational intensity>\n", argv0);
+    printf("%s --help --validate --load --store --memory <hwloc_obj:idx> --output <output> --with-hyperthreading --oi <operational intensity>\n", argv0);
     
     exit(EXIT_SUCCESS);
 }
@@ -37,6 +38,9 @@ void parse_args(int argc, char ** argv){
 	else if(!strcmp(argv[i],"--output") || !strcmp(argv[i],"-o")){
 	    output = argv[++i];
 	}
+	else if(!strcmp(argv[i],"--oi")){
+	    oi = atof(argv[++i]);
+	}
     }
     if(!load && !store){
 	load =1; store=1;
@@ -54,23 +58,25 @@ FILE * open_output(char * out){
 }
 
 void roofline_mem_bench(FILE * out, hwloc_obj_t memory){
-    double oi;
-
-    oi = 0;
+    double oi_local;
     /* Benchmark load */
     if(load){
 	roofline_bandwidth(out, memory, ROOFLINE_LOAD);
-	if(validate)
-	    for(oi = pow(2,-12); oi < pow(2,6); oi*=2){
-		roofline_oi(out, memory, ROOFLINE_LOAD, oi);
+	if(oi>0)
+	    roofline_oi(out, memory, ROOFLINE_LOAD, oi);
+	else if(validate)
+	    for(oi_local = pow(2,-12); oi_local < pow(2,6); oi_local*=2){
+		roofline_oi(out, memory, ROOFLINE_LOAD, oi_local);
 	    }
     }
     /* Benchmark store */
     if(store){
 	roofline_bandwidth(out, memory, ROOFLINE_STORE);
-	if(validate)
-	    for(oi = pow(2,-12); oi < pow(2,6); oi*=2){
-		roofline_oi(out, memory, ROOFLINE_STORE, oi);
+	if(oi>0)
+	    roofline_oi(out, memory, ROOFLINE_STORE, oi);
+	else if(validate)
+	    for(oi_local = pow(2,-12); oi_local < pow(2,6); oi*=2){
+		roofline_oi(out, memory, ROOFLINE_STORE, oi_local);
 	    }
     }
 }
