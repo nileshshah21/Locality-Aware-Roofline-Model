02dbd301a5569529f86f9455f96a3b90c2605039
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-04-26 13:42:36 +0200

    replace with more powerfull program

--- utils/omp_set_affinity.sh
+++
@@ -1,7 +0,0 @@
-#Openmp Bind to Cores
-export OMP_NUM_THREADS=$(lstopo --only Core --restrict `hwloc-calc Node:0`| wc -l)
-export GOMP_CPU_AFFINITY=""
-for i in $(seq 0 1 $(($OMP_NUM_THREADS-1))); do
-    GOMP_CPU_AFFINITY="$GOMP_CPU_AFFINITY $(lstopo --physical --restrict `hwloc-calc Core:$i` --only PU | awk -F'#' '{print $2; exit}')"
-done
-
