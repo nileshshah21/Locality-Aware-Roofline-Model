d80d58d52fef6e7c55fbcc79208937d989039dfa
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-09-05 14:15:57 +0200

    clean version

--- src/MSC/generic.c
+++ src/MSC/generic.c
@@ -6,275 +6,173 @@
 #include "../roofline.h"
 #include "MSC.h"
 
-#define ELEM_SIZE sizeof(ROOFLINE_STREAM_TYPE)
-#define VEC_LEN 8
-#define VEC_BYTES (VEC_LEN * ELEM_SIZE)
-#define VEC_FLOPS 16
+size_t chunk_size = 2;
+ROOFLINE_STREAM_TYPE res;
+#define LENGTH 128
+ROOFLINE_STREAM_TYPE flops[LENGTH];
+#define SIZE_T sizeof(ROOFLINE_STREAM_TYPE)
 
-#define scalar_add(a,b) (a + b)
-#define scalar_mul(a,b) (a * b)
-#define scalar_triad(a,b,c) (a+c*b)
-#define scalar_copy(dst, src) dst = src
-
-#define vec_add(a,b)				\
-    scalar_copy(a[0], scalar_add(a[0],b[0]));	\
-    scalar_copy(a[1], scalar_add(a[1],b[1]));	\
-    scalar_copy(a[2], scalar_add(a[2],b[2]));	\
-    scalar_copy(a[3], scalar_add(a[3],b[3]));	\
-    scalar_copy(a[4], scalar_add(a[4],b[4]));	\
-    scalar_copy(a[5], scalar_add(a[5],b[5]));	\
-    scalar_copy(a[6], scalar_add(a[6],b[6]));	\
-    scalar_copy(a[7], scalar_add(a[7],b[7]))
-
-#define vec_mul(a,b)				\
-    scalar_copy(a[0], scalar_mul(a[0],b[0]));	\
-    scalar_copy(a[1], scalar_mul(a[1],b[1]));	\
-    scalar_copy(a[2], scalar_mul(a[2],b[2]));	\
-    scalar_copy(a[3], scalar_mul(a[3],b[3]));	\
-    scalar_copy(a[4], scalar_mul(a[4],b[4]));	\
-    scalar_copy(a[5], scalar_mul(a[5],b[5]));	\
-    scalar_copy(a[6], scalar_mul(a[6],b[6]));	\
-    scalar_copy(a[7], scalar_mul(a[7],b[7]))
-
-#define vec_triad(a,b,c)					\
-    scalar_copy(a[0], scalar_triad((a)[0],(b)[0],(c)[0]));	\
-    scalar_copy(a[1], scalar_triad((a)[1],(b)[1],(c)[1]));	\
-    scalar_copy(a[2], scalar_triad((a)[2],(b)[2],(c)[2]));	\
-    scalar_copy(a[3], scalar_triad((a)[3],(b)[3],(c)[3]));	\
-    scalar_copy(a[4], scalar_triad((a)[4],(b)[4],(c)[4]));	\
-    scalar_copy(a[5], scalar_triad((a)[5],(b)[5],(c)[5]));	\
-    scalar_copy(a[6], scalar_triad((a)[6],(b)[6],(c)[6]));	\
-    scalar_copy(a[7], scalar_triad((a)[7],(b)[7],(c)[7]))
-
-#define vec_copy(dst,src)			\
-    scalar_copy((dst)[0],(src)[0]);		\
-    scalar_copy((dst)[1],(src)[1]);		\
-    scalar_copy((dst)[2],(src)[2]);		\
-    scalar_copy((dst)[3],(src)[3]);		\
-    scalar_copy((dst)[4],(src)[4]);		\
-    scalar_copy((dst)[5],(src)[5]);		\
-    scalar_copy((dst)[6],(src)[6]);		\
-    scalar_copy((dst)[7],(src)[7])
-
-size_t alloc_chunk_aligned(ROOFLINE_STREAM_TYPE ** data, size_t size){
-    size-=size%VEC_BYTES;
-    size+=VEC_BYTES;
-    int err;
-    if(data != NULL){
-	err = posix_memalign((void**)data, alignement, size);
-	switch(err){
-	case 0:
-	    break;
-	case EINVAL:
-	    fprintf(stderr,"The alignment argument was not a power of two, or was not a multiple of sizeof(void *).\n");
-	    break;
-	case ENOMEM:
-	    fprintf(stderr,"There was insufficient memory to fulfill the allocation request.\n");
-	}
-	if(*data == NULL)
-	    fprintf(stderr,"Chunk is NULL\n");
-	if(err || *data == NULL)
-	    errEXIT("");
-
-    	memset(*data,0,size);
-    }
-    return size;
+void mul(unsigned long reps){
+    unsigned long i, j;
+ for(i = 0; i<reps;i++){
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+     for(j=0;j<LENGTH;j++) flops[j] *= flops[j];
+ }
 }
 
-void load_bandwidth_bench(struct roofline_sample_in * in, struct roofline_sample_out * out){
-    ROOFLINE_STREAM_TYPE local_copy[VEC_LEN];
-    memset(local_copy,0,sizeof(local_copy));
-    volatile uint64_t c_low=0, c_low1=0, c_high=0, c_high1=0;
-    unsigned i, j, end = in->stream_size/sizeof(*(in->stream));
-
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
+void add(unsigned long reps){
+    unsigned long i,j;
+    for(i = 0; i<reps;i++){
+#ifdef _OPENMP
+#pragma omp parallel for
 #endif
-    roofline_rdtsc(c_high, c_low);
-    for(j=0; j<in->loop_repeat; j++){
-	for(i=0; i<end; i+=VEC_LEN){
-	    vec_copy(local_copy, &(in->stream[i]));
-	}
+	for(j=0;j<LENGTH;j++) flops[j] += flops[j];
     }
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
+}
+
+void mad(unsigned long reps){
+    unsigned long i, j;
+   for(i = 0; i<reps;i++){
+#ifdef _OPENMP
+#pragma omp parallel for
 #endif
+	for(j=0;j<LENGTH;j++) flops[j] += flops[j]*flops[j];
+    }
+}
+
+void fpeak_benchmark(const struct roofline_sample_in * in, struct roofline_sample_out * out, int type){
+    void (* bench)(unsigned long) = NULL;
+    unsigned i;
+    switch(type){
+    case ROOFLINE_MUL:
+	bench = mul;
+	out->flops = 8*in->loop_repeat;
+	break;
+    case ROOFLINE_MAD:
+	bench = mad;
+	out->flops = 8*in->loop_repeat*2;
+	break;
+    case ROOFLINE_ADD:
+	bench = mul;
+	out->flops = 16*in->loop_repeat;
+	break;
+    default:
+	return;
+	break;
+    }
+    
+    uint64_t c_low0=0, c_low1=0, c_high0=0, c_high1=0;
+    roofline_rdtsc(c_high0, c_low0);
+    bench(in->loop_repeat);
     roofline_rdtsc(c_high1, c_low1);
-    out->ts_start = roofline_rdtsc_diff(c_high, c_low);
+    out->ts_start = roofline_rdtsc_diff(c_high0, c_low0);
     out->ts_end = roofline_rdtsc_diff(c_high1, c_low1);
-    out->bytes = sizeof(local_copy[0]) * (end+1) * in->loop_repeat;
-    out->instructions = out->bytes/VEC_BYTES;
+    out->instructions = out->flops;
 }
 
-void store_bandwidth_bench(struct roofline_sample_in * in, struct roofline_sample_out * out){
-    ROOFLINE_STREAM_TYPE local_copy[VEC_LEN];
-    memset(local_copy,0,sizeof(local_copy));
-    uint64_t c_low=0, c_low1=0, c_high=0, c_high1=0;
-    unsigned i, j, end = in->stream_size/sizeof(*(in->stream));
-
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
+void load(ROOFLINE_STREAM_TYPE * src, size_t size){
+    unsigned i;
+    ROOFLINE_STREAM_TYPE res;
+#pragma vector temporal(src)
+#ifdef _OPENMP
+#pragma omp parallel for
 #endif
-    roofline_rdtsc(c_high, c_low);
-    for(j=0; j<in->loop_repeat; j++){
-	for(i=0; i<end; i+=VEC_LEN){
-	    vec_copy(&(in->stream[i]), local_copy);
-	}
-    }
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
-#endif
-    roofline_rdtsc(c_high1, c_low1);
-
-    out->ts_start = roofline_rdtsc_diff(c_high, c_low);
-    out->ts_end = roofline_rdtsc_diff(c_high1, c_low1);
-    out->bytes = in->stream_size * in->loop_repeat;
-    out->instructions = out->bytes/VEC_BYTES;
+    for(i=0;i<size;i++){res+=src[i];}
 }
 
-inline void fpeak_bench(struct roofline_sample_in * in, struct roofline_sample_out * out){
-    unsigned j;
-    uint64_t c_low=0, c_low1=0, c_high=0, c_high1=0;
-    ROOFLINE_STREAM_TYPE a[VEC_LEN];
-    ROOFLINE_STREAM_TYPE b[VEC_LEN];
-    ROOFLINE_STREAM_TYPE c[VEC_LEN];
-
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
+void load_nt(ROOFLINE_STREAM_TYPE * src, size_t size){
+    unsigned i;
+    ROOFLINE_STREAM_TYPE res;
+#ifdef __INTEL_COMPILER
+#pragma vector nontemporal(src)
 #endif
-    roofline_rdtsc(c_high, c_low);
-    for(j=0; j<in->loop_repeat; j++){
-	vec_triad(a,b,c);
-    }
-	
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
+#ifdef _OPENMP
+#pragma omp parallel for
 #endif
-    roofline_rdtsc(c_high1, c_low1);
-
-    out->ts_start = roofline_rdtsc_diff(c_high, c_low);
-    out->ts_end = roofline_rdtsc_diff(c_high1, c_low1);
-    out->instructions = in->loop_repeat;
-    out->flops = VEC_FLOPS * in->loop_repeat;
+    for(i=0;i<size;i++){res+=src[i];}
 }
 
-
-unsigned flops_per_mop;
-unsigned mops_per_flop;
-int oi_type;
-
-void oi_bench(struct roofline_sample_in * in, struct roofline_sample_out * out){
-    unsigned i,j,k, end = in->stream_size/sizeof(*(in->stream));
-    uint64_t c_low=0, c_low1=0, c_high=0, c_high1=0;
-    ROOFLINE_STREAM_TYPE a[VEC_LEN];
-    ROOFLINE_STREAM_TYPE b[VEC_LEN];
-    ROOFLINE_STREAM_TYPE c[VEC_LEN];
-    ROOFLINE_STREAM_TYPE stream0[VEC_LEN];
-    ROOFLINE_STREAM_TYPE stream1[VEC_LEN];
-    memset(stream0,0,sizeof(stream0));
-    memset(stream1,0,sizeof(stream1));
-    memset(a,0,sizeof(a));
-    memset(b,0,sizeof(b));
-    memset(c,0,sizeof(c));
-
-    if(flops_per_mop == 1){
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
+void store(ROOFLINE_STREAM_TYPE * src, size_t size){
+    unsigned i;
+    ROOFLINE_STREAM_TYPE res;
+#ifdef __INTEL_COMPILER
+#pragma vector temporal(src)
 #endif
-    roofline_rdtsc(c_high, c_low);
-    for(j=0; j<in->loop_repeat; j++){
-	for(i=0; i<end; i+=2*VEC_LEN){
-	    vec_copy(stream0, &(in->stream[i]));
-	    vec_triad(a,b,c);
-	    vec_copy(stream1, &(in->stream[2*i]));
-	}
-    }
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
+#ifdef _OPENMP
+#pragma omp parallel for
 #endif
-    roofline_rdtsc(c_high1, c_low1);
-    out->flops = VEC_FLOPS * end/(VEC_LEN);
-    }
-
-    else if(flops_per_mop > 1){
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
-#endif
-    roofline_rdtsc(c_high, c_low);
-    for(j=0; j<in->loop_repeat; j++){
-	for(i=0; i<end; i+=VEC_LEN){
-	    vec_copy(stream0, &(in->stream[i]));
-	    for(k=0; k<flops_per_mop; k++){
-		vec_triad(a,b,c);
-	    }
-	}
-    }
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
-#endif
-    roofline_rdtsc(c_high1, c_low1);
-    out->flops = VEC_FLOPS * flops_per_mop * end/(VEC_LEN);
-    }
-
-    else{
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
-#endif
-    roofline_rdtsc(c_high, c_low);
-    for(j=0; j<in->loop_repeat; j++){
-	for(i=0; i<end; i+=VEC_LEN){
-	    vec_copy(stream0, &(in->stream[i]));
-	    if(i%mops_per_flop == 0){
-		vec_triad(a,b,c);
-	    }
-	}
-    }
-#ifdef USE_OMP
-#pragma omp barrier
-#pragma omp master
-#endif
-    roofline_rdtsc(c_high1, c_low1);
-    out->flops = VEC_FLOPS * end / (VEC_LEN * mops_per_flop);
-    }
-
-    out->ts_start = roofline_rdtsc_diff(c_high, c_low);
-    out->ts_end = roofline_rdtsc_diff(c_high1, c_low1);
-    out->bytes = in->stream_size * in->loop_repeat;
-    out->instructions = out->bytes / VEC_BYTES + out->flops/VEC_FLOPS;
-    /* printf("Bandwidth = %f GByte/s\n", out->bytes * cpu_freq * 1e-9 / (out->ts_end - out->ts_start)); */
-    /* printf("Flops/Bytes = %f\n", (float)out->flops/(float)out->bytes); */
+    for(i=0;i<size;i++){src[i] = res;}
 }
 
-
-void (* roofline_oi_bench(double oi, int type))(struct roofline_sample_in * in, struct roofline_sample_out * out)
-{
-    if(oi==0 && type==ROOFLINE_LOAD) return load_bandwidth_bench;
-    if(oi==0 && type==ROOFLINE_STORE) return store_bandwidth_bench;
-
-    oi_type = type;
-    if(oi>1){
-	flops_per_mop = oi/2;
-	mops_per_flop = 0;
-    }
-    else if(oi==1){
-	flops_per_mop = 1;
-	mops_per_flop = 1;
-    }
-    else if(oi<1){
-	flops_per_mop = 0;
-	mops_per_flop = 2/oi;
-    }
-    return oi_bench;
+void store_nt(ROOFLINE_STREAM_TYPE * src, size_t size){
+    unsigned i;
+    ROOFLINE_STREAM_TYPE res;
+#ifdef __INTEL_COMPILER
+#pragma vector nontemporal(src)
+#endif
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+    for(i=0;i<size;i++){src[i] = res;}
 }
+
+void copy(ROOFLINE_STREAM_TYPE * src, size_t size){
+    unsigned i;
+    size_t half_size = size/2;
+    ROOFLINE_STREAM_TYPE * to = &(src[half_size]);
+#ifdef __INTEL_COMPILER
+#pragma vector nontemporal(src, to)
+#endif
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+    for(i=0;i<half_size;i++){to[i] = src[i];}
+}
+
+void bandwidth_benchmark(const struct roofline_sample_in * in, struct roofline_sample_out * out, int type){
+    unsigned i;
+    uint64_t c_low0=0, c_low1=0, c_high0=0, c_high1=0;
+
+    void (*bench)(ROOFLINE_STREAM_TYPE*, size_t) = NULL;
+    switch(type){
+    case ROOFLINE_LOAD:
+	bench = load;
+	break;
+    case ROOFLINE_LOAD_NT:
+	bench = load_nt;
+	break;
+    case ROOFLINE_STORE:
+	bench = store;
+	break;
+    case ROOFLINE_STORE_NT:
+	bench = store_nt;
+	break;
+    case ROOFLINE_COPY:
+	bench=copy;
+	break;
+    default:
+	return;
+	break;
+    }
+
+    roofline_rdtsc(c_high0, c_low0);
+    for(i = 0; i<in->loop_repeat; i++)
+	bench(in->stream, in->stream_size/SIZE_T);
+    roofline_rdtsc(c_high1, c_low1);
+    out->ts_start = roofline_rdtsc_diff(c_high0, c_low0);
+    out->ts_end = roofline_rdtsc_diff(c_high1, c_low1);    
+    out->bytes = in->stream_size*in->loop_repeat;
+    out->instructions = in->loop_repeat*in->stream_size/SIZE_T;
+ }
+
+void (*roofline_oi_bench(__attribute__ ((unused)) const double oi, __attribute__ ((unused)) const int type)) (const struct roofline_sample_in * in, struct roofline_sample_out * out, int type){
+    return NULL;
+}
+
+ int benchmark_types_supported(){
+    return ROOFLINE_LOAD|ROOFLINE_LOAD_NT|ROOFLINE_STORE|ROOFLINE_STORE_NT|ROOFLINE_COPY|ROOFLINE_MUL|ROOFLINE_ADD|ROOFLINE_MAD;
+}
+
 
