aa3681fd162f4665fb6c55d59b627f7d39d8d702
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2016-11-08 15:34:45 +0100

    do not print logical index for caches

--- src/topology.c
+++ src/topology.c
@@ -74,10 +74,13 @@ int roofline_hwloc_obj_snprintf(hwloc_obj_t obj, char * info_in, size_t n){
   int nc;
   memset(info_in,0,n);
   /* Special case for MCDRAM */
-  if(obj->type == HWLOC_OBJ_NUMANODE && obj->subtype != NULL && !strcmp(obj->subtype, "MCDRAM"))
+  if(obj->type == HWLOC_OBJ_NUMANODE && obj->subtype != NULL && !strcmp(obj->subtype, "MCDRAM")){
     nc = snprintf(info_in, n, "%s", obj->subtype);
+  }
   else nc = hwloc_obj_type_snprintf(info_in, n, obj, 0);
-  nc += snprintf(info_in+nc,n-nc,":%d ",obj->logical_index);
+  if((int)obj->depth <= hwloc_get_type_depth(topology, HWLOC_OBJ_NUMANODE)){
+    nc += snprintf(info_in+nc,n-nc,":%d ",obj->logical_index);
+  }
   return nc;
 }
 
@@ -318,9 +321,6 @@ hwloc_obj_t roofline_hwloc_get_next_memory(hwloc_obj_t obj){
   
   /* get parent memory */
   do{obj=hwloc_get_obj_inside_cpuset_by_depth(topology, root->cpuset, obj->depth-1, 0);} while(obj!=NULL && !roofline_hwloc_obj_is_memory(obj));
-  /* If obj is a above NUMANode, then take the topology left most obj */
-  if(obj != NULL && (int)obj->depth <= hwloc_get_type_depth(topology, HWLOC_OBJ_NUMANODE))
-    obj = hwloc_get_obj_by_depth(topology, obj->depth, 0);
   
   /* No memory left in topology */
   return obj;
