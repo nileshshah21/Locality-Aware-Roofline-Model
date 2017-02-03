ae1e442307ba115cbb519f795e0895bc30fd0f74
denoyelle.nicolas@gmail.com <denoyelle.nicolas@gmail.com>
2017-02-03 15:26:57 +0100

    add textual informations

--- utils/plot_roofs.R
+++ utils/plot_roofs.R
@@ -52,12 +52,12 @@ args = commandArgs(trailingOnly = TRUE)
 optParse = OptionParser(option_list = c(inOpt, outOpt, datOpt, validOpt, titleOpt, locationOpt, bandwidthOpt, fpeakOpt, statOpt, dfilterOpt))
 options = parse_args(optParse, args=args)
 
-# options$input = "~/Documents/LARM/output/Xeon_E5-2650L_v4/joe0.roofs"
-# options$data = "~/Documents/LARM/output/Xeon_E5-2650L_v4/kernels.roofs"
-# options$validation = T
-# options$stats = T
-# options$bandwidth="NUMANode:0|2LD1ST,NUMANode:0|LOAD,NUMANode:1|2LD1ST,NUMANode:1|LOAD"
-# options$filter="load|triad,load|ddot"
+## options$input = "~/Documents/LARM/output/Xeon_E5-2650L_v4/joe0.roofs"
+## options$data = "~/Documents/LARM/output/Xeon_E5-2650L_v4/kernels.roofs"
+## options$validation = T
+## options$stats = T
+## options$bandwidth="NUMANode:0|2LD1ST,NUMANode:0|LOAD,NUMANode:1|2LD1ST,NUMANode:1|LOAD"
+## options$filter="load|triad,load|ddot"
 
 #################################################################################################################################
 ## Process options
@@ -108,22 +108,25 @@ if(is.null(options$title)){options$title=basename(options$input)}
 ## Extract synthetic data
 #################################################################################################################################
 
-fpeak_roofs <- function(df, types=NULL){
-  fpeaks = data.frame(type=character(0), perf=numeric(0), sd=numeric(0), nthreads=integer(0), stringsAsFactors=FALSE)
+fpeak_roofs <- function(df, types=NULL, verbose=T){
+  if(verbose){print("Performance peaks:")}
+  fpeaks = data.frame(type=character(0), GFlop.s=numeric(0), sd=numeric(0), nthreads=integer(0), stringsAsFactors=FALSE)
   samples = df[is.na(df$memory),]
   if(is.null(types)){types = unique(samples$type)} 
   
   for(i in 1:length(types)){
     s = samples[samples$type==types[i],]
     if(nrow(s) <= 0){next}
-    ns = data.frame(type=types[i], perf=median(s$GFlop.s), sd=sd(s$GFlop.s), nthreads=s$n_threads[1], stringsAsFactors=F)
+    ns = data.frame(type=types[i], GFlop.s=median(s$GFlop.s), sd=sd(s$GFlop.s), nthreads=s$n_threads[1], stringsAsFactors=F)
     fpeaks = rbind(fpeaks,ns)
   }
+  if(verbose){print(fpeaks); cat("\n")}
   fpeaks
 }
 
-bandwidth_roofs <- function(df, types=NULL){
-  bandwidths = data.frame(obj=character(0), type=character(0), val=numeric(0), sd=numeric(0), nthreads=integer(0), stringsAsFactors=FALSE)
+bandwidth_roofs <- function(df, types=NULL, verbose=T){
+  if(verbose){print("Bandwidths:")}
+  bandwidths = data.frame(obj=character(0), type=character(0), GByte.s=numeric(0), sd=numeric(0), nthreads=integer(0), stringsAsFactors=FALSE)
   samples = df[df$GFlop.s==0,]
   
   if(is.null(types)){
@@ -138,37 +141,53 @@ bandwidth_roofs <- function(df, types=NULL){
   for(type in types){
     s = samples[samples$memory==type[1] & samples$type==type[2],]
     if(nrow(s) <= 0){next}
-    ns = data.frame(obj=as.character(type[1]), type=as.character(type[2]), val=median(s$GByte.s), sd=sd(s$GByte.s), nthreads=s$n_threads[1], stringsAsFactors=F)
+    ns = data.frame(obj=as.character(type[1]), type=as.character(type[2]), GByte.s=median(s$GByte.s), sd=sd(s$GByte.s), nthreads=s$n_threads[1], stringsAsFactors=F)
     bandwidths = rbind(bandwidths, ns)
   }
+  if(verbose){print(bandwidths); cat("\n")}
   bandwidths
 }
 
-validation_points <- function(df, type, obj){
-  ret = data.frame(oi=numeric(0), 
-                   perf=numeric(0), 
+rrmse <-function(y.valid, y.roof){
+  y.max = max(y.roof)
+  relative_error = (y.valid-y.roof)*(y.max-y.valid)/y.max
+  relative_root_mean_squared_error = sqrt(sum(relative_error*relative_error))/length(y.valid)
+}
+
+validation_points <- function(df, type){
+  ret = new.env()
+  ret$points = data.frame(oi=numeric(0), 
+                   GFlop.s=numeric(0), 
                    sd=numeric(0), 
-                   info=character(0),
+                   obj=character(0),
                    type=character(0),
                    stringsAsFactors=FALSE)
-  samples = df[!is.infinite(df$Flops.Byte) & df$Flops.Byte>0 & df$type==type & df$memory==obj,]
+  
+  samples = df[!is.infinite(df$Flops.Byte) & df$Flops.Byte>0 & df$type==type[2] & df$memory==type[1],]
   ois = unique(samples$Flops.Byte)
   for(oi in ois){
     s = samples[samples$Flops.Byte==oi,]
     if(nrow(s) <= 0){next}
-    ns = data.frame(oi=oi, perf=median(s$GFlop.s), sd=sd(s$GFlop.s), info=obj, type=type, stringsAsFactors=FALSE)
-    ret = rbind(ret,ns)
+    ns = data.frame(oi=oi, GFlop.s=median(s$GFlop.s), sd=sd(s$GFlop.s), obj=type[1], type=type[2], stringsAsFactors=FALSE)
+    ret$points = rbind(ret$points,ns)
   }
+  
+  bandwidth = bandwidth_roofs(df, list(type), verbose=F)
+  fpeak = max(df$GFlop.s)
+  roof = sapply(1:nrow(ret$points), function(i){min(c(bandwidth$GByte.s[1]*ret$points$oi[i], fpeak))})
+  ret$error = data.frame(memory=type[1], op_type=type[2], fpeak.GFlops.s=fpeak, error.GFlop.s=rrmse(ret$points$GFlop.s, roof), stringsAsFactors=FALSE)
+  ## Error of validation points computed with: (sqrt(sum((y-y')*((y-y'max)/y'max))^2)/length(y))" 
+  
   ret
 }
 
-data_points <- function(pt, types=NULL){
+data_points <- function(pt, types=NULL, verbose=T){
   if(is.null(pt)){return(NULL)}
   i=1
   ret = data.frame(oi=numeric(0), 
-                   perf=numeric(0), 
-                   time=numeric(0),
+                   GFlop.s=numeric(0), 
                    sd=numeric(0), 
+                   time=numeric(0),                   
                    info=character(0), 
                    type=character(0), 
                    stringsAsFactors=FALSE)
@@ -186,14 +205,16 @@ data_points <- function(pt, types=NULL){
     s = pt[pt$info==type[2] & pt$type==type[1],]
     if(nrow(s) <= 0){next}
     ns = data.frame(oi=median(s$Flops/s$Bytes), 
-                    perf=median(s$Flops/s$Nanoseconds), 
-                    time=median(s$Nanoseconds),
+                    GFlop.s=median(s$Flops/s$Nanoseconds), 
                     sd=sd(s$Flops/s$Nanoseconds), 
+                    time=median(s$Nanoseconds),
                     info=as.character(type[2]), 
                     type=as.character(type[1]), 
                     stringsAsFactors=FALSE)
     ret = rbind(ret, ns)
   }
+  
+  if(verbose){print(ret); cat("\n")}
   ret
 }
 
@@ -210,16 +231,16 @@ plot_bandwidths <- function(bandwidths, fpeak_max, xlim, ylim, col_start=1){
   oi = lseq(xlim[1], xlim[2], 500)
   
   for(i in 1:nrow(bandwidths)){
-    val = bandwidths$val[i]
-    perf = sapply(oi*val, min, fpeak_max)
-    plot(oi, perf, lty=1, type="l", log="xy", axes=FALSE, xlab="", ylab="", xlim=xlim, ylim=ylim, col=col_start+i-1)
+    GByte.s = bandwidths$GByte.s[i]
+    GFlop.s = sapply(oi*GByte.s, min, fpeak_max)
+    plot(oi, GFlop.s, lty=1, type="l", log="xy", axes=FALSE, xlab="", ylab="", xlim=xlim, ylim=ylim, col=col_start+i-1)
     par(new=T)
     if(options$stats){
       sd = bandwidths$sd[i]
-      a0.x = oi[1];                  a0.y = oi[1]*(val-sd*0.5)
-      a1.x = oi[1];                  a1.y = oi[1]*(val+sd*0.5)
-      a2.x = fpeak_max/(val+sd*0.5); a2.y = fpeak_max
-      a3.x = fpeak_max/(val-sd*0.5); a3.y = fpeak_max
+      a0.x = oi[1];                  a0.y = oi[1]*(GByte.s-sd*0.5)
+      a1.x = oi[1];                  a1.y = oi[1]*(GByte.s+sd*0.5)
+      a2.x = fpeak_max/(GByte.s+sd*0.5); a2.y = fpeak_max
+      a3.x = fpeak_max/(GByte.s-sd*0.5); a3.y = fpeak_max
       coord.x = c(a0.x, a1.x, a2.x, a3.x)
       coord.y = c(a0.y, a1.y, a2.y, a3.y)
       polygon(coord.x,coord.y,col=adjustcolor(col_start+i-1,alpha.f=.25), lty="blank")
@@ -230,17 +251,17 @@ plot_bandwidths <- function(bandwidths, fpeak_max, xlim, ylim, col_start=1){
 
 plot_fpeaks <- function(fpeaks, xmin){
   for(i in 1:nrow(fpeaks)){
-    abline(h = fpeaks$perf[i], lty=3, col=1, lwd=2);
-    axis(2, labels = fpeaks$type[i], at=fpeaks$perf[i], las=1, tick=FALSE, pos=xmin, padj=0, hadj=0)
+    abline(h = fpeaks$GFlop.s[i], lty=3, col=1, lwd=2);
+    axis(2, labels = fpeaks$type[i], at=fpeaks$GFlop.s[i], las=1, tick=FALSE, pos=xmin, padj=0, hadj=0)
   }
   par(new=T)
 }
 
-plot_validation <-function(pts, col=1, pch=1, cex=.4){
-  points(pts$oi, pts$perf, pch=pch, col=col, cex=cex)
+plot_validation <-function(pts, col=1, pch=1, cex=.5){
+  points(pts$oi, pts$GFlop.s, pch=pch, col=col, cex=cex)
   par(new=T)
   if(options$stats){
-    segments(x0 = pts$oi, x1 = pts$oi, y0 = pts$perf-pts$sd*0.5, y1=pts$perf+pts$sd*0.5, col=col, lty=1)
+    segments(x0 = pts$oi, x1 = pts$oi, y0 = pts$GFlop.s-pts$sd*0.5, y1=pts$GFlop.s+pts$sd*0.5, col=col, lty=1)
   }
 }
 
@@ -251,19 +272,19 @@ plot_data <-function(pts, xlim, ylim, col_start=1, pch_start=1, cex=1){
   
   for(i in 1:nrow(pts)){
     r = pts$time[i]*0.1/tmax
-    symbols(pts$oi[i], pts$perf[i], circles=1, inches=r, add=T, fg=colors[i], bg=adjustcolor(colors[i],alpha.f=.25), xlim=xlim, ylim=ylim, lwd=.5)
-    points(pts$oi[i], pts$perf[i], pch=1, col=colors[i], cex=.3)
+    symbols(pts$oi[i], pts$GFlop.s[i], circles=1, inches=r, add=T, fg=colors[i], bg=adjustcolor(colors[i],alpha.f=.25), xlim=xlim, ylim=ylim, lwd=.5)
+    points(pts$oi[i], pts$GFlop.s[i], pch=1, col=colors[i], cex=.3)
     if(options$stats){
-      segments(x0 = pts$oi[i], x1 = pts$oi[i], y0 = pts$perf[i]-pts$sd[i]*0.5, y1=pts$perf[i]+pts$sd[i]*0.5, col=colors[i], lty=1)
+      segments(x0 = pts$oi[i], x1 = pts$oi[i], y0 = pts$GFlop.s[i]-pts$sd[i]*0.5, y1=pts$GFlop.s[i]+pts$sd[i]*0.5, col=colors[i], lty=1)
     }
   }
   
   legend("topright", legend=lgd, cex=.7, lty=1, col=colors, bg="white")
 }
 
-roofline_plot <- function(df, bandwidths, fpeaks, validation=F, data=NULL){
+roofline_plot <- function(df, bandwidths, fpeaks, validation=F, data=NULL, verbose=T){
   #Set limit and ticks
-  fpeak_max = max(fpeaks$perf)
+  fpeak_max = max(fpeaks$GFlop.s)
   xmin = 2^-8; xmax = 2^6; xlim = c(xmin,xmax)
   xticks = lseq(xmin,xmax,log(xmax/xmin,base=2) + 1)
   xlabels = sapply(xticks, function(i) as.expression(bquote(2^ .(round(log(i,base=2))))))
@@ -283,13 +304,19 @@ roofline_plot <- function(df, bandwidths, fpeaks, validation=F, data=NULL){
   
   #plot validation points
   if(validation){
-    invisible(sapply(1:nrow(bandwidths), function(i){plot_validation(validation_points(df, bandwidths$type[i], bandwidths$obj[i]), col=i)}))
+    errors = data.frame(memory=character(0), op_type=character(0), fpeak.GFlops.s=numeric(0), error.GFlop.s=numeric(0), stringsAsFactors=FALSE)
+    for(i in 1:nrow(bandwidths)){
+      valid = validation_points(df, c(bandwidths$obj[i], bandwidths$type[i]))
+      plot_validation(valid$points, col=i)
+      errors = rbind(errors, valid$error)
+    }
+    if(verbose){print(errors); cat("\n")}
   }
   
   #plot layout
   axis(1, at=xticks, labels=xlabels)
   for (i in 1:nrow(fpeaks)){
-    j=1; flops = as.numeric(fpeaks$perf[i])
+    j=1; flops = as.numeric(fpeaks$GFlop.s[i])
     while(j <= length(yticks)){
       if( (yticks[j]<2*flops) && (yticks[j]>flops) ){
         yticks = yticks[-c(j)]
@@ -304,12 +331,12 @@ roofline_plot <- function(df, bandwidths, fpeaks, validation=F, data=NULL){
       j = j+1
     }
   }
-  yticks = c(yticks, fpeaks$perf)
-  ylabels = c(ylabels, sprintf("%.2f", as.numeric(fpeaks$perf)))
+  yticks = c(yticks, fpeaks$GFlop.s)
+  ylabels = c(ylabels, sprintf("%.2f", as.numeric(fpeaks$GFlop.s)))
   axis(2, at=yticks, labels=ylabels, las=1)
   title(main = sprintf("%s (%s)", options$title, df$location[1]), xlab="Flops/Byte", ylab="GFlops/s")
   legend("bottomright",
-         legend=sprintf("%s %s=%.2fGB/s", bandwidths$obj, bandwidths$type, bandwidths$val),
+         legend=sprintf("%s %s=%.2fGB/s", bandwidths$obj, bandwidths$type, bandwidths$GByte.s),
          cex=.7, lty=1, col=1:nrow(bandwidths), bg="white")
   
   #plot data points
@@ -317,9 +344,12 @@ roofline_plot <- function(df, bandwidths, fpeaks, validation=F, data=NULL){
 }
 
 for(loc in unique(df$location)){
+  cat(sprintf("Process location: %s", loc), "\n\n")
   roofs = df[df$location==loc,]
   appli = NULL; if(!is.null(pt)){appli = pt[pt$Location==loc,]}
-  pdf(sprintf("%s.%s", gsub(":", "-",loc), options$output), onefile=T, family = "Helvetica", basename(options$input), width=10, height=5)
+  file_output = sprintf("%s.%s", gsub(":", "-",loc), options$output)
+  pdf(file_output, onefile=T, family = "Helvetica", basename(options$input), width=10, height=5)
   roofline_plot(roofs, bandwidth_roofs(roofs, types=bandwidth_types), fpeak_roofs(roofs), options$validation, data_points(appli))
+  cat(sprintf("Output to %s", file_output), "\n")
   graphics.off()
 }
